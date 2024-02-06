57b691cee99cbfd25208993e18481df1b587bd12
Jaron Swab <jaron@swab.dev>
2024-02-05 21:43:44 -0500

    turned off relative line numbers

--- lua/swabian/set.lua
+++ lua/swabian/set.lua
@@ -1,7 +1,7 @@
 vim.opt.guicursor = ""
 
 vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -12,21 +12,27 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+-- Use Undo tree instead of Vim's swap/backup:
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- Highligting on searches
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+-- Don't let cursor move within scrolloff number:
+vim.opt.scrolloff = 4
+
+-- Show "signs" in a column to the left of the code:
 vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
+-- How often Vim checks for changes in the current buffer:
+vim.opt.updatetime = 50 --milliseconds
 
+-- Be real programer...
 vim.opt.colorcolumn = "80"
 
